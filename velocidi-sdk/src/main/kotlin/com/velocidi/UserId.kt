f19f929f21cfb6268d5d46137201d1f4c7547a9e
dimamo5 <diogomoura13@gmail.com>
2021-03-10 14:05:34 +0000

    Ensure UserId cannot be instantiated with empty fields

--- velocidi-sdk/src/main/kotlin/com/velocidi/UserId.kt
+++ velocidi-sdk/src/main/kotlin/com/velocidi/UserId.kt
@@ -6,7 +6,13 @@ package com.velocidi
  * @property type type of identifier (defaults to "gaid")
  * @property id the identifier
  * @see <a href="https://docs.velocidi.com/collect/user-ids">https://docs.velocidi.com/collect/user-ids</a>
+ * @throws IllegalArgumentException Throws when id or type is empty
  */
 data class UserId(val id: String, val type: String = "gaid") {
+    init {
+        require(id.isNotEmpty()) { "id cannot be empty" }
+        require(type.isNotEmpty()) { "type cannot be empty" }
+    }
+
     fun toPair(): Pair<String, String> = Pair("id_$type", id)
 }
